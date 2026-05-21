ddafba3f32e37d9434f3d9e1cc4ba92bf438136a
gcunhapoatek <gabriel.cunha@poatek.com>
2026-05-21 01:07:41 -0300

    Show hidden + gitignored files in neo-tree
    Default neo-tree filters hide dotfiles and gitignored entries, which
    makes editing this repo unusable: every package has a .config/
    subdir and most actual configs start with a dot. Set visible=true
    so they render dimmed instead of disappearing, and explicitly hide
    .git/ and .DS_Store. Also enable follow_current_file.

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,12 +1,26 @@
 return {
-    {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        "nvim-tree/nvim-web-devicons", -- optional, but recommended
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    lazy = false,
+    opts = {
+      filesystem = {
+        filtered_items = {
+          -- Show hidden + gitignored files (dimmed) instead of hiding them.
+          -- Essential for editing dotfiles repos where every interesting
+          -- file lives under .config/ or starts with a dot.
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = { ".DS_Store", ".git" },
+        },
+        follow_current_file = { enabled = true },
       },
-      lazy = false, -- neo-tree will lazily load itself
-    }
-  }
+    },
+  },
+}
